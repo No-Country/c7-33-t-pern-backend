f1d813dba1a7ee30aba88e10160031079f128a66
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-09-29 15:03:16 -0500

    Interfaces types

--- src/interfaces/types.d.ts
+++ src/interfaces/types.d.ts
@@ -1,7 +1,32 @@
-export interface DataBase {
+import { Model } from 'sequelize'
+import { Optional } from 'sequelize/types'
+export interface DataBaseType {
   host: string
   name: string
   password: string
   port: number
   user: string
+}
+
+type role = 'admin' | 'client'
+type status = 'active' | 'inactive'
+export interface UserAttributes{
+  id: number
+  name: string
+  lastName: string
+  email: string
+  password: string
+  role: role
+  status: status
+  avatar: string
+}
+
+export type UserCreationAttributes = Optional<UserAttributes, 'id' | 'name'| 'avatar'| 'role'| 'status'| 'lastName'>
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user: Model<UserAttributes, UserCreationAttributes>
+    }
+  }
 }
