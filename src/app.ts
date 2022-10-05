69e29e56f81fe056e26707bde0222253ddd3ba2d
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-10-05 16:22:57 -0500

    update: app, interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
-import usersRouter from './routes/userRouter'
 import { globalErrorHandler } from './controllers/error.controller'
+import routers from './routes'
 
 // Init our Express app
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use('/api/v1/users', usersRouter)
+app.use('/api/v1', routers)
 app.use(globalErrorHandler)
 
 // Catch non-existing endpoints
